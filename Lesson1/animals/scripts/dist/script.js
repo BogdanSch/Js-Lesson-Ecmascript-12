31986afd7c201ffe18dd87d9580bfc89eeb19673
Bohdan <bogmira777@gmail.com>
2024-02-24 12:27:33 +0100

    Fix code compiling

--- Lesson1/animals/scripts/dist/script.js
+++ Lesson1/animals/scripts/dist/script.js
@@ -2,12 +2,16 @@
 
 var _dog = require("./modules/dog.js");
 var _cat = require("./modules/cat.js");
-var content = document.querySelector(".content");
+// const content = document.querySelector(".content");
+
 var dog = new _dog.Dog("Buddy", 4, "Golden Retriever");
 var cat = new _cat.Cat("Whiskers", 3, "Siamese");
-content.innerHTML = "";
+
+// content.innerHTML = "";
+
 function displayContent(message) {
-  content.innerHTML += "<p>".concat(message, "</p>");
+  //content.innerHTML += `<p>${message}</p>`;
+  console.log("".concat(message, "\n"));
 }
 displayContent(dog.walk());
 displayContent(cat.walk());
--- Lesson1/animals/scripts/src/script.js
+++ Lesson1/animals/scripts/src/script.js
@@ -1,15 +1,16 @@
 import { Dog } from "./modules/dog.js";
 import { Cat } from "./modules/cat.js";
 
-const content = document.querySelector(".content");
+// const content = document.querySelector(".content");
 
 const dog = new Dog("Buddy", 4, "Golden Retriever");
 const cat = new Cat("Whiskers", 3, "Siamese");
 
-content.innerHTML = "";
+// content.innerHTML = "";
 
 function displayContent(message) {
-  content.innerHTML += `<p>${message}</p>`;
+  //content.innerHTML += `<p>${message}</p>`;
+  console.log(`${message}\n`);
 }
 
 displayContent(dog.walk());
